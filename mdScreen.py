9027df9a51579160c18236d77d247254541af422
KumaTea <KumaTea@outlook.com>
2021-11-18 19:20:27 +0800

    optimize NGA

--- mdScreen.py
+++ mdScreen.py
@@ -7,9 +7,10 @@ def get_screenshot(url, delay=1):
     driver.get(url)
     if 'nga' in url:
         images = driver.find_elements_by_xpath('//button[normalize-space()="显示图片"]')
-        if len(images) == 1:
-            images[0].click()
+        for image in images:
+            image.click()
     sleep(delay)
+    driver.execute_script("window.scrollTo(0, 0);")  # scroll to top
     screenshot = driver.get_screenshot_as_png()
     return BytesIO(screenshot)
 
