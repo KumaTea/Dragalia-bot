8520113d2a4f500e8cca3fd8fd46fa7246871e4a
Baoshuo Ren <i@baoshuo.ren>
2022-04-27 06:44:42 +0800

    chore: remove git.io
    All links on git.io will stop redirecting after April 29, 2022.
    
    - https://github.blog/changelog/2022-04-25-git-io-deprecation/

--- info.py
+++ info.py
@@ -5,6 +5,6 @@ version = '3.0.0.0b0'
 channel = 'cloud'
 
 developer_id = 345060487
-repo = 'https://git.io/JvDw4'
+repo = 'https://github.com/KumaTea/Dragalia-bot'
 
 player_group = -1001157490282  # @dragaliazh
